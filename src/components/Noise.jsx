ffa0250b5379d697cfcc3c2b4307f72e1ada0e0b
goldic <contact@goldic.xyz>
2025-03-18 23:25:41 +0400

    Remove test components, create noise texture

---
+++ src/components/Noise.jsx
@@ -0,0 +1,22 @@
+import NoiseImg from "../assets/noise.png";
+
+const Noise = ({ opacity = 1 }) => {
+  return (
+    <div
+      className="absolute inset-0 pointer-events-none z-10"
+      style={{
+        backgroundImage: `url(${NoiseImg})`,
+        backgroundRepeat: "repeat",
+        mixBlendMode: "soft-light",
+        position: "absolute",
+        top: 0,
+        left: 0,
+        width: "100%",
+        height: "100%",
+        opacity: opacity,
+      }}
+    ></div>
+  );
+};
+
+export default Noise;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,28 +1,9 @@
 import FullPageScroll from "../components/FullPageScroll";
-import Sec from "../components/Sec";
+import Hero from "../components/sections/Hero";
 const Home = () => {
   return (
     <FullPageScroll>
-      <div className="bg-green-300 h-full w-full flex flex-col items-center justify-center">
-        <h1 className="text-4xl mb-4">Section 1</h1>
-        <p>Scroll or use the child's skip function from next sections!</p>
-      </div>
-
-      <div className="bg-blue-300 h-full w-full">
-        <Sec />
-      </div>
-
-      <div className="bg-purple-300  h-full w-full flex items-center justify-center">
-        <h1 className="text-4xl">Section 3</h1>
-      </div>
-
-      <div className="bg-yellow-300  h-full w-full flex items-center justify-center">
-        <h1 className="text-4xl">Section 4 (target of the skip)</h1>
-      </div>
-
-      <div className="bg-red-300 h-full w-full flex items-center justify-center">
-        <h1 className="text-4xl">Section 5</h1>
-      </div>
+      <Hero />
     </FullPageScroll>
   );
 };
